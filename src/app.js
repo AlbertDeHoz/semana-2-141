f4947301006e4d554947870e3a23f102dc49b301
EEVM2020 <eevalenciano@misena.edu.co>
2020-12-04 23:20:31 -0500

    se sube el modulo de miembros usando vue

---
+++ src/app.js
@@ -0,0 +1,7 @@
+var vm = new Vue({
+    data: {
+      // declare message with an empty value
+      member: ''
+    },
+   
+  })
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+//import TeamCard from './components/TeamCard.vue'
 
 
 Vue.use(VueAxios, axios)
@@ -11,5 +12,8 @@ Vue.config.productionTip = false
 
 new Vue({
     store,
-    render: h => h(App)
+    render: h => h(App),
+    
+     
+  
 }).$mount('#app')
